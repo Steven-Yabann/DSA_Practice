82285d81d06339919c915e5b6339e177f961c8b5
Steven-Yabann <stevekiptoo1@gmail.com>
2024-07-03 23:27:03 +0300

    Fully implemented the stack structure and created its functions e.g. pop and push

--- src/DSA_Practice/Stacks.java
+++ src/DSA_Practice/Stacks.java
@@ -1,5 +1,46 @@
 package DSA_Practice;
 
 public class Stacks {
+    private static class Node{
+        int data;
+        Node next;
+        private Node(int data){
+            this.data = data;
+        }
+    }
+    Node top;
+    public boolean isEmpty(){
+        return top == null;
+    }
+    public int peek(){
+        return top.data;
+    }
+    public void push(int data){
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty!");
+        }
+        int poppedData = top.data;
+        top = top.next;
+        return poppedData;
+    }
 
+    public static void main(String[] args) {
+        Stacks stack = new Stacks();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println(stack.peek()); // Should print 30
+        System.out.println(stack.pop()); // Should print 30
+        System.out.println(stack.pop()); // Should print 20
+        System.out.println(stack.pop()); // Should print 10
+
+        System.out.println(stack.isEmpty());
+    }
 }
