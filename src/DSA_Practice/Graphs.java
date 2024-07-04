ea8262d24fdbd1415edfaf3aebe6739a53ca2f57
Steven-Yabann <stevekiptoo1@gmail.com>
2024-07-04 23:42:43 +0300

    Added Depth First Search

--- src/DSA_Practice/Graphs.java
+++ src/DSA_Practice/Graphs.java
@@ -1,17 +1,16 @@
 package DSA_Practice;
+import java.util.*;
 
 public class Graphs {
-
+    Random random = new Random();
     // inner class to track edges
     private class Edge {
         char src, dest;
+        int weight;
     }
-
-    private int vertices, edges; // number of edges and vertices
+    private int vertices, edges; // number of vertices, edges
     private int edgeCount;      // track number of edges as we build the graph
-
     private Edge[] edge;
-
     // constructor
     public Graphs(int vertices, int edges) {
         this.vertices = vertices;
@@ -24,29 +23,49 @@ public class Graphs {
             edge[i] = new Edge();
         }
     }
-
     public void addEdge(char src, char dest) {
         if (edgeCount < edges) {
             edge[edgeCount].src = src;
+            edge[edgeCount].weight = random.nextInt(10);
             edge[edgeCount].dest = dest;
             edgeCount++;
         } else {
             System.out.println("Cannot add any more items, the static graph is full");
         }
     }
-
     // printing method
     public void printGraph() {
         for (int i = 0; i < edgeCount; i++) {
-            System.out.println(edge[i].src + " -- " + edge[i].dest);
+            System.out.println(edge[i].src + " -- " + edge[i].weight + " -- " + edge[i].dest);
         }
     }
+    public void DFS(char start){
+        Set<Character> visited = new HashSet<>();
+        Stack<Character> stack = new Stack<>();
 
+        stack.push(start);
+
+        while(!stack.isEmpty()) {
+            char vertex = stack.pop();
+
+            if(!visited.contains(vertex)){
+                visited.add(vertex);
+                System.out.println(vertex + " ");
+
+                for(int i = 0; i < edgeCount; i++){
+                    if(edge[i].src == vertex && !visited.contains(edge[i].dest)){
+                        stack.push(edge[i].dest);
+                    }else if(edge[i].dest == vertex && !visited.contains(edge[i].src)){
+                        stack.push(edge[i].src);
+                    }
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int noVertices = 6;
         int noEdges = 9;
         Graphs g = new Graphs(noVertices, noEdges);
-
         // adding edges
         g.addEdge('A', 'B');
         g.addEdge('A', 'C');
@@ -59,15 +78,8 @@ public class Graphs {
         g.addEdge('D', 'E');
 
         g.printGraph();
-
+        System.out.println("Depth First Traversal starting from vertex A:");
+        g.DFS('A');
     }
-
-
 }
-
-
-
-
-
-
 
